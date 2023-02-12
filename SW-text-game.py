60b20ac5616d30021bf4ee2766f372c93829d7fb
Anakin Crywalker <112024003+dibukae@users.noreply.github.com>
2023-02-12 04:24:30 +0000

    welcome

--- SW-text-game.py
+++ SW-text-game.py
@@ -1,2 +1,2 @@
 print("Welcome to Star Wars: Save the Chancellor!")
-print("In this game you must save Chancellor Palpatine who has been captured by the Separatists.")
+print("In this game your main objective is to save Chancellor Palpatine, who has been captured by the Separatists.")
