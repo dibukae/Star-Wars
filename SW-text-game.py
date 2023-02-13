7db2d4d4e866d494159412ed01c6d42c944179cb
Anakin Crywalker <112024003+dibukae@users.noreply.github.com>
2023-02-13 21:58:03 +0000

    part1

--- SW-text-game.py
+++ SW-text-game.py
@@ -1,4 +1,6 @@
 print("Welcome to Star Wars: Save the Chancellor!")
 print("In this game your main objective is to save Chancellor Palpatine, who has been captured by the Separatists.\n")
-print("Here is a key to navigate in the game.")
+plyrName = input("Enter your epic Jedi name:\n")
+
+print(f"Greetings, {plyrName}! Here is a key to navigate in the game.")
 print("Key: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory,")
