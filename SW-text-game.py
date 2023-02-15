8d540a2f0124258c535cb54aecde9b73c2951b72
Anakin Crywalker <112024003+dibukae@users.noreply.github.com>
2023-02-15 23:00:10 +0000

    battle

--- SW-text-game.py
+++ SW-text-game.py
@@ -17,7 +17,7 @@ print(f"Greetings, {plyrName}! Here is a key to navigate in the game.")
 input("KEY: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory, save, load\nPress enter to continue.")
 
 input(f"\nHello there, Jedi {plyrName}. We need your help to save Chancellor Palpatine.\nHe has been captured by the Separatists. It is rumored that he was captured by the leader of the droid army, General Grievous.")
-print("\nPrepare your starfighter and be prepared to board his ship.")
+print("\nPrepare your starfighter and be prepared to board his ship. Good luck! And may the force be with you!")
 input("<You start up your starfighter and fly over to the the Separatist Frigate.>")
 
 print("\nThere is only one way in, through the hangar.")
@@ -48,4 +48,4 @@ if pathOption == "north" or "n":
 					defeatedDroids += 1
 					input(f"\nYou defeated {defeatedDroids} droids!")
 		if defeatedDroids == 3:
-			print("You defeated all the droids!")
+			print("\nYou defeated all the droids!")
