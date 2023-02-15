645a846d1b291be1232a37a989e36c9f1f2d9a05
Anakin Crywalker <112024003+dibukae@users.noreply.github.com>
2023-02-15 19:42:13 +0000

    part1

--- SW-text-game.py
+++ SW-text-game.py
@@ -3,6 +3,6 @@ print("In this game your main objective is to save Chancellor Palpatine, who has
 plyrName = input("Enter your epic Jedi name:\n")
 
 print(f"Greetings, {plyrName}! Here is a key to navigate in the game.")
-print("Key: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory,")
+print("Key: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory, save, load")
 
 print(f"\nHello there, Jedi {plyrName}. We need your help to save Chancellor Palpatine. He has been captured by the Separatists. It is rumored that he was captured by the leader of the droid army, General Grievous.")
