41642483f37e1131b9f429678894f573031ad95c
Anakin Crywalker <112024003+dibukae@users.noreply.github.com>
2023-02-15 20:00:46 +0000

    part1

--- SW-text-game.py
+++ SW-text-game.py
@@ -3,7 +3,7 @@ print("In this game your main objective is to save Chancellor Palpatine, who has
 plyrName = input("Enter your epic Jedi name:\n")
 
 print(f"Greetings, {plyrName}! Here is a key to navigate in the game.")
-input("Key: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory, save, load\nPress enter to continue.")
+input("KEY: north/n, south/s, east/e, west/w, up, down, flee, fight, use, take, inventory, save, load\nPress enter to continue.")
 
 input(f"\nHello there, Jedi {plyrName}. We need your help to save Chancellor Palpatine.\nHe has been captured by the Separatists. It is rumored that he was captured by the leader of the droid army, General Grievous.")
 print("\nPrepare your starfighter and be prepared to board his ship.")
@@ -11,6 +11,7 @@ input("<You start up your starfighter and fly over to the the Separatist Frigate
 
 print("\nThere is only one way in, through the hangar.")
 pathOption = input("Available paths: north\n")
+print()
 if pathOption == "north" or "n":
 	print("You swoop into the hangar and quickly exit your ship.\nSuddenly, a large group of Separatist battle droids are surrounding you. They have been awaiting your arrival.")
 	fleeOrfight = input("Do you flee or fight?:\n")
